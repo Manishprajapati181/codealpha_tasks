42f64eeb63f11a1342c192c55bafac83ebc45c45
Manish Prajapati <prajapatimanish8968@gmail.com>
2026-01-05 11:07:57 +0530

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,25 @@
+def chatbot_response(user_input):
+    user_input = user_input.lower()
+
+    if user_input == "hello":
+        return "Hi!"
+    elif user_input == "how are you":
+        return "I'm fine, thanks!"
+    elif user_input == "bye":
+        return "Goodbye!"
+    else:
+        return "Sorry, I don't understand."
+
+def run_chatbot():
+    print("Basic Chatbot")
+    print("Type 'hello', 'how are you', or 'bye' to exit.\n")
+
+    while True:
+        user_input = input("You: ")
+        response = chatbot_response(user_input)
+        print("Bot:", response)
+
+        if user_input.lower() == "bye":
+            break
+
+run_chatbot()
